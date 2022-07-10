874c781d541f299690d2171701b03f26cfa4b9fb
Johny Carvajal <john.carvajal@duocuc.cl>
2022-07-09 21:09:14 -0400

    Modificacion y agregacion de acciones en carro

--- Examen/AmigosPeludos/cart/cart.py
+++ Examen/AmigosPeludos/cart/cart.py
@@ -10,21 +10,46 @@ class Cart:
         if not cart:
             cart = self.session["cart"] = {}
         self.cart = cart
-        #creacion del producto en el carro
-        def add(self, producto):
-            if str(producto.id) not in self.cart.keys():
-                self.cart[producto.id] = {
-                    "producto_id": producto.id,
-                    "nombre": producto.name,
-                    "cantidad": 1,
-                    "precio": str(producto.precio),
-                    "imagen":producto.imagen.url  
-                    }    
-            #Si el producto ya existe
-            else:
-                for key , value in  self.cart.items():
-                  if key == str(producto.id) :
-                    value["quantity"] = value["quantity"] + 1
-                    self.save()     
+    #creacion del producto en el carro
+    def add(self, producto):
+        if str(producto.id) not in self.cart.keys():
+            self.cart[producto.id] = {
+                "producto_id": producto.id,
+                "nombre": producto.name,
+                "cantidad": 1,
+                "precio": str(producto.precio),
+                "imagen":producto.imagen.url  
+                }    
+        #Si el producto ya existe
+        else:
+            for key , value in  self.cart.items():
+                if key == str(producto.id) :
+                    value["quantity"] = value["quantity"] + 1    
                     break
-                  
+
+        self.save()    
+    #definir guardar
+    def save(self):
+        self.session["cart"] = self.cart
+        #actualiza sesion en carro
+        self.session.modified = True
+
+    #borrar un producto del carro
+    def remove(self,producto):
+        producto_id = str(producto.id)
+        if producto_id in self.cart:
+            del self.cart[producto_id]
+            self.save()
+
+    #disminuir cantidad de productos
+
+    def decrement(self, producto):
+        for key, value in self.cart.items():
+            if key == str(producto.id):
+                value["quantity"] = value["quantity"] - 1
+                if value["quantity"] < 1:
+                    self.remove(producto)
+                self.save()
+                break
+            else:
+                print("Producto no existe en tu carro de compras")
